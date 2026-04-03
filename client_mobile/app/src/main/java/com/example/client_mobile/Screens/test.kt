db7c1608f6d492771030cb2a9fc1d0fc2edbcbf6
tqolh <tarik.project22@gmail.com>
2026-04-04 00:49:48 +0100

    test

---
+++ client_mobile/app/src/main/java/com/example/client_mobile/Screens/test.kt
@@ -0,0 +1,10 @@
+package com.example.client_mobile.Screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+
+fun test(){
+    Text(   "Git branch ")
+}
